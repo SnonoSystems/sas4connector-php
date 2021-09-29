c1149dc6b313e7f9efb3b7cf0c94f982ff607f45
hasanen <hasanen@snono-systems.com>
2021-09-29 10:51:58 +0300

    updates examples

--- test.php
+++ test.php
@@ -1,14 +1,14 @@
 <?php
 
 require_once 'SASConnector.php';
-
+//connect to admin portal and list users
 $acp = new SASConnector('demo4.sasradius.com', 'admin', 'admin', 'acp');
 $acp->login();
 $res = $acp->post('index/user', []);
 
 print_r(json_decode($res));
 
-
+//Connect to user portal and fetch user info
 $ucp = new SASConnector('demo4.sasradius.com', 'demo1', '1234', 'ucp');
 $ucp->login();
 $res = $ucp->get('user');
